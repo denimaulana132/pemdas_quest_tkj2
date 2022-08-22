a1951886459d73820154bb7e5fa88bdd8460120c
deni <111479855+denimaulana132@users.noreply.github.com>
2022-08-21 21:56:58 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,15 @@
+nd=input("nama depan:")
+nb=input("nama belakang:")
+print("nama anda adalah",nd,nb)
+
+a=int(input("masukan nilai A:"))
+b=float(input("masukan nilai B:"))
+c=a+b
+
+print("A=", a)
+print("B", b)
+print(a,"+",b,"=",c)
+
+x=int(input("masukan nilai mata uang: "))
+y= x * 13752
+print(x,"dollar=",y,"rupiah")
